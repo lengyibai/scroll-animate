9414d18b4ce5ba079af16da8ea9eb9489d5db4a0
lengyibai <1329670984@qq.com>
2022-01-03 03:51:49 +0800

    500px更改为200%、新增元素滚动动画

--- scroll-animat.js
+++ scroll-animat.js
@@ -1,14 +1,14 @@
 function translate1(el) {
-  el.style.transform = `translateY(-500px)`;
+  el.style.transform = `translateY(-200%)`;
 }
 function translate2(el) {
-  el.style.transform = 'translateY(500px)';
+  el.style.transform = 'translateY(200%)';
 }
 function translate3(el) {
-  el.style.transform = 'translateX(-500px)';
+  el.style.transform = 'translateX(-200%)';
 }
 function translate4(el) {
-  el.style.transform = 'translateX(500px)';
+  el.style.transform = 'translateX(200%)';
 }
 function rotate1(el) {
   el.style.transform = 'rotateX(180deg)';
@@ -26,55 +26,55 @@ function scale3(el) {
   el.style.transform = 'scale3d(2,1,1)';
 }
 function mixedT1(el) {
-  el.style.transform = 'translateX(-500px) translateY(500px)';
+  el.style.transform = 'translateX(-200%) translateY(200%)';
 }
 function mixedT2(el) {
-  el.style.transform = 'translateX(-500px) translateY(-500px)';
+  el.style.transform = 'translateX(-200%) translateY(-200%)';
 }
 function mixedT3(el) {
-  el.style.transform = 'translateX(500px) translateY(-500px)';
+  el.style.transform = 'translateX(200%) translateY(-200%)';
 }
 function mixedT4(el) {
-  el.style.transform = 'translateX(500px) translateY(500px)';
+  el.style.transform = 'translateX(200%) translateY(200%)';
 }
 function mixedTR1(el) {
-  el.style.transform = 'translateY(-500px) rotateX(180deg)';
+  el.style.transform = 'translateY(-200%) rotateX(180deg)';
 }
 function mixedTR2(el) {
-  el.style.transform = 'translateY(500px) rotateX(-180deg)';
+  el.style.transform = 'translateY(200%) rotateX(-180deg)';
 }
 function mixedTR3(el) {
-  el.style.transform = 'translateX(-500px) rotateY(180deg)';
+  el.style.transform = 'translateX(-200%) rotateY(180deg)';
 }
 function mixedTR4(el) {
-  el.style.transform = 'translateX(500px) rotateY(-180deg)';
+  el.style.transform = 'translateX(200%) rotateY(-180deg)';
 }
 function mixedTR5(el) {
-  el.style.transform = 'translateX(-500px) rotateZ(-180deg)';
+  el.style.transform = 'translateX(-200%) rotateZ(-180deg)';
 }
 function mixedTR6(el) {
-  el.style.transform = 'translateX(500px) rotateZ(180deg)';
+  el.style.transform = 'translateX(200%) rotateZ(180deg)';
 }
 function mixedTS1(el) {
-  el.style.transform = 'translateX(-500px) scale(0.1)';
+  el.style.transform = 'translateX(-200%) scale(0.1)';
 }
 function mixedTS2(el) {
-  el.style.transform = 'translateX(500px) scale(0.1)';
+  el.style.transform = 'translateX(200%) scale(0.1)';
 }
 function mixedTS3(el) {
-  el.style.transform = 'translateX(500px) scale(2)';
+  el.style.transform = 'translateX(200%) scale(2)';
 }
 function mixedTS4(el) {
-  el.style.transform = 'translateX(-500px) scale(2)';
+  el.style.transform = 'translateX(-200%) scale(2)';
 }
 function mixedTS5(el) {
-  el.style.transform = 'translateY(-500px) scale(2)';
+  el.style.transform = 'translateY(-200%) scale(2)';
 }
 function mixedTS6(el) {
-  el.style.transform = 'translateY(500px) scale(0.1)';
+  el.style.transform = 'translateY(200%) scale(0.1)';
 }
 function mixedTS7(el) {
-  el.style.transform = 'translateY(500px) scale(2)';
+  el.style.transform = 'translateY(200%) scale(2)';
 }
 function mixedRS1(el) {
   el.style.transform = 'rotateZ(180deg) scale(0.1)';
@@ -86,40 +86,70 @@ function opacity(el) {
   el.style.opacity = 0;
 }
 
-function Animats(el, obj = {}) {
-  let {
-    x = '-200%',
-    y = 0,
-    rx = 0,
-    ry = 0.01,
-    rz = 0.01,
-    sx = 1,
-    sy = 1,
-  } = obj;
-  el.style.transform = `translateX(${x}) translateY(${y}) rotateX(${rx}deg)  rotateY(${ry}deg)  rotateZ(${rz}deg) scaleX(${sx}) scaleY(${sy})`;
+function Animats(el, obj = {}, percentage = null) {
+  let { x = 0, y = 0, rx = 0, ry = 0, rz = 0, sx = 0.5, sy = 0.5 } = obj;
+  if (percentage == null) {
+    el.style.transform = `translateX(${x}) translateY(${y}) rotateX(${rx}deg) rotateY(${ry}deg) rotateZ(${rz}deg) scaleX(${sx}) scaleY(${sy})`;
+  } else {
+    if (x.includes('px')) {
+      x = x.replace('px', '');
+      el.style.transform = `translateX(${x - percentage * x}%) translateY(${
+        y - percentage * y
+      }%)`;
+    } else if (x.includes('%')) {
+      x = x.replace('%', '');
+      el.style.transform = `translateX(${x - percentage * x}%) translateY(${
+        y - percentage * y
+      }%)`;
+    }
+  }
 }
 
 function $lybS2(obj, animats) {
-  let { el, animat = Animats, time = 1, playY = '50%' } = obj;
+  let {
+    el,
+    animat = Animats,
+    time = 0.5,
+    playY = '50%',
+    scrollFllow = false,
+    PLAYY = '100%',
+    fade = 0,
+  } = obj;
   function lyb(item, animat, time) {
-    // 判断是否进入可视区
-    if (
+    // 触发时机高度转为数字
+    playY =
+      typeof playY == 'number'
+        ? playY
+        : item.offsetHeight * (playY.replace('%', '') / 100);
+    // 获取元素进入可视区的距离
+    let entryHeight =
       document.documentElement.clientHeight -
-        item.getBoundingClientRect().top -
-        item.offsetHeight -
-        (typeof playY == 'number'
-          ? playY
-          : item.offsetHeight * (playY.replace('%', '') / 100)) >=
-      -item.offsetHeight
-    ) {
+      item.getBoundingClientRect().top -
+      playY;
+    // 判断是否进入达到触发动画的范围
+    if (entryHeight >= 0) {
+      // 判断是否开启了跟随滚动
+      if (scrollFllow) {
+        let percentage =
+          entryHeight /
+          (typeof PLAYY == 'number'
+            ? PLAYY
+            : item.offsetHeight * (PLAYY.replace('%', '') / 100));
+        percentage > 1 && (percentage = 1);
+        // 进入后播放动画归位
+        item.style.transition = 'all 0s';
+        fade || (item.style.opacity = percentage);
+        animat(item, animats, percentage);
+        return;
+      }
       // 进入后播放动画归位
-      item.style.opacity = 1;
-      item.style.transform = 'translateX(0%) rotate(0deg) scale(1)';
-    } else {
-      // 否则回到进入前的位置
       item.style.transition = `all ${time}s`;
-      item.style.transitionDelay = '0s';
-      item.style.opacity = 0;
+      item.style.opacity = 1;
+      item.style.transform = null;
+      // 如果离开了可视区，回到进入前的位置
+    } else if (entryHeight + playY < 0) {
+      item.style.transition = `all 0s`;
+      item.style.opacity = fade;
       animat(item, animats);
     }
   }
